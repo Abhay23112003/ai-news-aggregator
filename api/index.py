b559d0ac53dd5525f48b36bdf477efc9607921c5
Abhay Sharma <abhayliveyourlife@example.com>
2026-01-15 20:43:42 +0530

    adding in api to read neon

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
+from app.services.database import get_recent_articles
 
 app = FastAPI(title="AI News API")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/articles")
+def articles(limit:int=Query(10,le=1,ge=10)):
+    articles=get_recent_articles(limit=limit)
+    return {
+        "count":len(articles),
+        "articles":articles
+    }
